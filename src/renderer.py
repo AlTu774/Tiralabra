ea2af1ea6add882991dea230fa1bdb99aa64d84e
AlTu774 <81103520+AlTu774@users.noreply.github.com>
2023-03-26 21:46:48 +0300

    Create renderer.py

---
+++ src/renderer.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Renderer():
+    def __init__(self, display, display_size, map):
+        self.display = display
+        self.display_size = display_size
+        self.map = map
+        self.map_size = len(self.map)
+    
+    def render_map(self):
+        tile_size = self.display_size//self.map_size
+        print(tile_size)
+        for y in range(0, self.map_size):
+            for x in range(0, self.map_size):
+                pygame.draw.rect(self.display, self.map[y][x].color, (y*tile_size, x*tile_size, tile_size, tile_size))
