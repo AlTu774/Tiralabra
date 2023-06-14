13c63b30c136b8a911c94ec8c9a1f91b63b2b1b7
Alisa <ally@DESKTOP-EM566EE.localdomain>
2023-06-14 21:01:36 +0300

    comments added

--- src/loop.py
+++ src/loop.py
@@ -2,6 +2,20 @@ import pygame
 from tile import Tile
 
 class Loop():
+    """Class that handles user inputs and events.
+
+    Attributes:
+    map: matrix of map that contains tiles
+    tile_size: dimensions of a tile
+    len: map lenght
+    start: indicates whether start point has been placed on the map, bool
+    start_node: the starting node, tile
+    end: indicates whether ending point has been placed on the map, bool
+    end_node: the end node, tile
+    switch: switches the search to the other, bool
+    ani: True if animation is enable, False if it is disabled
+      """
+    
     def __init__(self, map, tile_size):
         self.map = map
         self.tile_size = tile_size
@@ -10,7 +24,6 @@ class Loop():
         self.start_node = None
         self.end = False
         self.end_node = None
-        self.down = False
         self.switch = 0
         self.ani = True
     
@@ -60,6 +73,7 @@ class Loop():
                 self.map[y][x].color = (200,200,200)
     
     def clear_map(self):
+        """function that clears map and resets start and end points."""
         map_size = len(self.map)
         for i in range(map_size):
             for j in range(map_size):
